16d4be2c1a1db88d41ee6bfadddd1a94d4feb0cc
Neil de Carteret <neil@lumphammer.com>
2022-01-12 16:51:13 +0000

    pico/openprops

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,43 +3,30 @@
 	import '../app.css';
 </script>
 
-<Header />
+<nav class="container-fluid">
+	<ul>
+		<li>Lumphammer</li>
+	</ul>
+	<ul>
+		<li>
+			<a href="http://github.com/n3dst4">GitHub</a>
+		</li>
+		<li>
+			<a href="http://gitlab.com/n3dst4">GitLab</a>
+		</li>
+		<li>
+			<a href="http://twitter.com/n3dst4">Twitter</a>
+		</li>
+	</ul>
+	<!-- <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li> -->
+</nav>
 
-<main>
+<main class="container">
 	<slot />
 </main>
 
 <footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
 </footer>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
 </style>
--- src/routes/thingy.svelte
+++
@@ -1,3 +0,0 @@
-<div>
-  thingy
-</div>
