77b018b6bc0c958d10fdde2da361e9869e0930d1
Neil de Carteret <neil@lumphammer.com>
2022-01-22 12:20:31 +0000

    wibbly gradients

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,32 @@
+<nav class="container-fluid">
+	<ul>
+		<li>Lumphammer</li>
+	</ul>
+	<ul>
+		<li>
+			<a href="http://github.com/n3dst4">GitHub</a>
+		</li>
+		<li>
+			<a href="http://gitlab.com/n3dst4">GitLab</a>
+		</li>
+		<li>
+			<a href="http://twitter.com/n3dst4">Twitter</a>
+		</li>
+		<!-- <li>
+			{#if scheme  === "light" }
+				<button class="secondary" title="Use dark mode" on:click={() => { scheme = "dark" }}>
+					<Moon16/>
+				</button>
+			{:else if scheme === "dark"}
+				<button class="secondary" title="Use light mode" on:click={() => { scheme = "light" }}>
+					<Sun16/>
+				</button>
+			{:else}
+				<button class="secondary" disabled title="Theme switcher disabled">
+					<Sun16/>
+				</button>
+			{/if}
+		</li> -->
+	</ul>
+	<!-- <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li> -->
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+// import Nav from "$lib/Nav.svelte";
+
 	import "../stylesheets/main.scss";
 	// import Sun16 from "svelte-octicons/lib/Sun16.svelte";
 	// import Moon16 from "svelte-octicons/lib/Moon16.svelte";
@@ -27,39 +29,7 @@
 	<div class="backdrop3" ></div>
 </div>
 
-
-<nav class="container-fluid">
-	<ul>
-		<li>Lumphammer</li>
-	</ul>
-	<ul>
-		<li>
-			<a href="http://github.com/n3dst4">GitHub</a>
-		</li>
-		<li>
-			<a href="http://gitlab.com/n3dst4">GitLab</a>
-		</li>
-		<li>
-			<a href="http://twitter.com/n3dst4">Twitter</a>
-		</li>
-		<!-- <li>
-			{#if scheme  === "light" }
-				<button class="secondary" title="Use dark mode" on:click={() => { scheme = "dark" }}>
-					<Moon16/>
-				</button>
-			{:else if scheme === "dark"}
-				<button class="secondary" title="Use light mode" on:click={() => { scheme = "light" }}>
-					<Sun16/>
-				</button>
-			{:else}
-				<button class="secondary" disabled title="Theme switcher disabled">
-					<Sun16/>
-				</button>
-			{/if}
-		</li> -->
-	</ul>
-	<!-- <li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li> -->
-</nav>
+<!-- <Nav /> -->
 
 <main class="container">
 	<slot />
