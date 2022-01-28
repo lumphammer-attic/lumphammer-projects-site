aeaae557e1046558ab9393976473f965074176b7
Neil de Carteret <neil@lumphammer.com>
2022-01-28 17:46:07 +0000

    blending etc

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,7 @@
 <script context="module" lang="ts">
 // import { browser } from "$app/env";
 import KoFi from "$lib/KoFi.svelte";
+import Lorem from "$lib/lorem.svelte";
 // export const prerender = true;
 // export const hydrate = false;
 
@@ -12,45 +13,22 @@ import KoFi from "$lib/KoFi.svelte";
 
 
 
-	<h1 data-text="lumphammer.net">
-		lumphammer.net
-	</h1>
+<h1 data-text="lumphammer.net">
+	lumphammer.net
+</h1>
 
-	<p>
-		This is my personal projects site (I'm not currently looking for new employment, thanks!)
-	</p>
+<p>
+	This is my personal projects site (I'm not currently looking for new employment, thanks!)
+</p>
 
-	<div class="grid">
-		<div>1</div>
-		<div>2</div>
-		<div>3</div>
-		<div>4</div>
-	</div>
+<div class="grid">
+	<div>1</div>
+	<div>2</div>
+	<div>3</div>
+	<div>4</div>
+</div>
 
-	<!-- <KoFi /> -->
+<!-- <KoFi /> -->
 
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et erat ut ex rhoncus vulputate. Vestibulum aliquam molestie molestie. Nunc a venenatis lectus, nec dignissim massa. Morbi venenatis, magna nec sagittis fermentum, enim tellus dignissim libero, id aliquet metus sem a purus. Quisque risus eros, imperdiet quis arcu at, tempor gravida ante. Nulla facilisi. Mauris eleifend, enim pharetra efficitur interdum, metus dolor ultricies tortor, id convallis tortor augue sit amet libero. Proin blandit efficitur congue. Nullam nisi ligula, molestie quis sagittis ut, accumsan nec quam. Etiam sapien diam, bibendum a mi in, sollicitudin interdum enim. Praesent iaculis eu odio eget aliquam. Nam urna ligula, aliquam vel finibus eu, semper id nunc. Phasellus aliquet vel neque vel viverra. 	
-	</div>
-
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et erat ut ex rhoncus vulputate. Vestibulum aliquam molestie molestie. Nunc a venenatis lectus, nec dignissim massa. Morbi venenatis, magna nec sagittis fermentum, enim tellus dignissim libero, id aliquet metus sem a purus. Quisque risus eros, imperdiet quis arcu at, tempor gravida ante. Nulla facilisi. Mauris eleifend, enim pharetra efficitur interdum, metus dolor ultricies tortor, id convallis tortor augue sit amet libero. Proin blandit efficitur congue. Nullam nisi ligula, molestie quis sagittis ut, accumsan nec quam. Etiam sapien diam, bibendum a mi in, sollicitudin interdum enim. Praesent iaculis eu odio eget aliquam. Nam urna ligula, aliquam vel finibus eu, semper id nunc. Phasellus aliquet vel neque vel viverra. 	
-	</div>
-
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et erat ut ex rhoncus vulputate. Vestibulum aliquam molestie molestie. Nunc a venenatis lectus, nec dignissim massa. Morbi venenatis, magna nec sagittis fermentum, enim tellus dignissim libero, id aliquet metus sem a purus. Quisque risus eros, imperdiet quis arcu at, tempor gravida ante. Nulla facilisi. Mauris eleifend, enim pharetra efficitur interdum, metus dolor ultricies tortor, id convallis tortor augue sit amet libero. Proin blandit efficitur congue. Nullam nisi ligula, molestie quis sagittis ut, accumsan nec quam. Etiam sapien diam, bibendum a mi in, sollicitudin interdum enim. Praesent iaculis eu odio eget aliquam. Nam urna ligula, aliquam vel finibus eu, semper id nunc. Phasellus aliquet vel neque vel viverra. 	
-	</div>
-
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et erat ut ex rhoncus vulputate. Vestibulum aliquam molestie molestie. Nunc a venenatis lectus, nec dignissim massa. Morbi venenatis, magna nec sagittis fermentum, enim tellus dignissim libero, id aliquet metus sem a purus. Quisque risus eros, imperdiet quis arcu at, tempor gravida ante. Nulla facilisi. Mauris eleifend, enim pharetra efficitur interdum, metus dolor ultricies tortor, id convallis tortor augue sit amet libero. Proin blandit efficitur congue. Nullam nisi ligula, molestie quis sagittis ut, accumsan nec quam. Etiam sapien diam, bibendum a mi in, sollicitudin interdum enim. Praesent iaculis eu odio eget aliquam. Nam urna ligula, aliquam vel finibus eu, semper id nunc. Phasellus aliquet vel neque vel viverra. 	
-	</div>
-
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et erat ut ex rhoncus vulputate. Vestibulum aliquam molestie molestie. Nunc a venenatis lectus, nec dignissim massa. Morbi venenatis, magna nec sagittis fermentum, enim tellus dignissim libero, id aliquet metus sem a purus. Quisque risus eros, imperdiet quis arcu at, tempor gravida ante. Nulla facilisi. Mauris eleifend, enim pharetra efficitur interdum, metus dolor ultricies tortor, id convallis tortor augue sit amet libero. Proin blandit efficitur congue. Nullam nisi ligula, molestie quis sagittis ut, accumsan nec quam. Etiam sapien diam, bibendum a mi in, sollicitudin interdum enim. Praesent iaculis eu odio eget aliquam. Nam urna ligula, aliquam vel finibus eu, semper id nunc. Phasellus aliquet vel neque vel viverra. 	
-	</div>
-
-	<div>
-		Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis et erat ut ex rhoncus vulputate. Vestibulum aliquam molestie molestie. Nunc a venenatis lectus, nec dignissim massa. Morbi venenatis, magna nec sagittis fermentum, enim tellus dignissim libero, id aliquet metus sem a purus. Quisque risus eros, imperdiet quis arcu at, tempor gravida ante. Nulla facilisi. Mauris eleifend, enim pharetra efficitur interdum, metus dolor ultricies tortor, id convallis tortor augue sit amet libero. Proin blandit efficitur congue. Nullam nisi ligula, molestie quis sagittis ut, accumsan nec quam. Etiam sapien diam, bibendum a mi in, sollicitudin interdum enim. Praesent iaculis eu odio eget aliquam. Nam urna ligula, aliquam vel finibus eu, semper id nunc. Phasellus aliquet vel neque vel viverra. 	
-	</div>
-
+<Lorem />
 
