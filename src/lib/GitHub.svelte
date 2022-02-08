e2a66ed5faa18c23652902f48245f69712f4a96f
Neil de Carteret <neil@lumphammer.com>
2022-02-08 19:13:55 +0000

    .

--- src/lib/GitHub.svelte
+++ src/lib/GitHub.svelte
@@ -1,4 +1,11 @@
+<script>
+  import Icon from 'svelte-awesome';
+  import github from 'svelte-awesome/icons/github';
+</script>
+
 <section class="interactive">
+  <Icon class="backdrop-icon" data={github} scale={3} />
   <h1>GitHub</h1>
   <a class="linkifier" href="https://github.com/n3dst4" target="_new">GitHub</a>
+  <p>See my projects on GitHub</p>
 </section>
--- src/lib/GitLab.svelte
+++ src/lib/GitLab.svelte
@@ -1,4 +1,11 @@
+<script>
+	import Icon from 'svelte-awesome';
+	import gitlab from 'svelte-awesome/icons/gitlab';
+</script>
+
 <section class="interactive">
-  <h1>Gitlab</h1>
-  <a class="linkifier" href="https://gitlab.com/n3dst4" target="_new">GitLab</a>
+  <Icon class="backdrop-icon" data={gitlab} scale={3} />
+	<h1>GitLab</h1>
+	<a class="linkifier" href="https://gitlab.com/n3dst4" target="_new">GitLab</a>
+  <p>See my projects on GitLab</p>
 </section>
