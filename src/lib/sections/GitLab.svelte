2b9c29e0e9ad07c0236b492a05e363d34f33f3d7
Neil de Carteret <neil@lumphammer.com>
2022-02-10 12:11:27 +0000

    .

--- src/lib/sections/GitLab.svelte
+++ src/lib/sections/GitLab.svelte
@@ -5,7 +5,7 @@
 
 <section class="interactive">
   <Icon class="backdrop-icon" data={gitlab} scale={3} />
-	<h2>GitLab</h2>
+	<h2>gitlab.com/n3dst4</h2>
 	<a class="linkifier" href="https://gitlab.com/n3dst4" target="_new">gitlab.com/n3dst4</a>
   <p>See my projects on GitLab</p>
 </section>
--- src/lib/sections/Twitter.svelte
+++ src/lib/sections/Twitter.svelte
@@ -5,7 +5,7 @@
 
 <section class="interactive">
   <Icon class="backdrop-icon" data={twitter} scale={3} />
-	<h2>Twitter</h2>
+	<h2>twitter.com/n3dst4</h2>
 	<a class="linkifier" href="https://twitter.com/n3dst4" target="_new">twitter.com/n3dst4</a>
   <p>See my aimless RTs on twitter dot com</p>
 </section>
