138cf7684b6fd44b38349138f818c7979fbb2c36
Neil de Carteret <git@lumphammer.net>
2023-02-15 19:07:17 +0000

    svelte-migrate: renamed files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script context="module" lang="ts">
+	throw new Error("@migration task: Check code was safely removed (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292722)");
+
+	// import Frontdrops from "$lib/Frontdrops.svelte";
+	// import "../stylesheets/main.scss";
+</script>
+
+<svelte:head>
+	<title>lumphammer projects</title>
+</svelte:head>
+
+<main>
+	<slot />
+</main>
+
+<Frontdrops />
---
+++ src/routes/+layout.ts
@@ -0,0 +1,2 @@
+import Frontdrops from "$lib/Frontdrops.svelte";
+import "../stylesheets/main.scss";
